999b0bfce23feca112b56aaf2f5ff40074ebe105
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-19 21:15:16 -0800

    updated static web files and html, added models - not tested!

--- OR_web_GUI/models.py
+++ OR_web_GUI/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Inputs(models.Model):
+    """Inputs from anything"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_used = models.DateTimeField()
+
+    def __str__(self):
+        """return a string representation of the model."""
+        return self.text
+
+
+class Outputs(models.Model):
+    """outputs to pins/relays"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_used = models.DateTimeField()
+
+    def __str__(self):
+        """return a string representation of the model."""
+        return self.text
+
+
+class Rule(models.Model):
+    """what when what"""
+    input = models.ForeignKey(Inputs)
+    output = models.ForeignKey(Outputs)
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_used = models.DateTimeField()
+
+    def __str__(self):
+        """return a string representation of the model."""
+        return self.text
