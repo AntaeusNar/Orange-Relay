d9813d5bd9033d6d91b86cc5496ada302088b19d
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-01-04 08:57:41 -0800

    improved the extendJSON handling

--- OR_web_GUI/extendJSON.py
+++ OR_web_GUI/extendJSON.py
@@ -1,12 +1,25 @@
 import json
 
 
-def writeJSONfile(path, fileName, data):
-    filePathNameWExt = './' + path + '/' + fileName + '.json'
-    with open(filePathNameWExt, 'w') as fp:
+def file_path_name_w_ext(file_name, path=None):
+    if path is None:
+        file_path = './'
+    else:
+        file_path = './' + path + '/'
+
+    if file_name.endswith(".json"):
+        path_name_w_ext = file_path + file_name
+    else:
+        path_name_w_ext = file_path + file_name + '.json'
+
+    return path_name_w_ext
+
+
+def writeJSONfile(file_name, data, path=None):
+    with open(file_path_name_w_ext(file_name, path), 'w') as fp:
         json.dump(data, fp)
 
 
-def getJSONfile(filePathAndName):
-    with open(filePathAndName, 'r') as fp:
+def getJSONfile(file_name, path=None):
+    with open(file_path_name_w_ext(file_name, path), 'r') as fp:
         return json.load(fp)
