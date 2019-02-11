0d0573270247ed44404356853e777be0c729eabf
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-02-11 15:23:28 -0800

    built out models for 3 way many-to-many added simpleeval to requirments

--- OR_web_GUI/models.py
+++ OR_web_GUI/models.py
@@ -26,7 +26,10 @@ class Output(models.Model):
         return self.text+" channel: "+str(self.channel)
 
 
+
+
 class Rule(models.Model):
+    # todo: rebuild the rule models to allow for many to many inputs and outputs and rule logic
     """what when what"""
     input = models.ForeignKey(Input, on_delete=models.CASCADE)
     output = models.ForeignKey(Output, on_delete=models.CASCADE)
@@ -37,3 +40,26 @@ class Rule(models.Model):
     def __str__(self):
         """return a string representation of the model."""
         return self.text
+
+
+# Start of rules v2
+
+
+class EvalExpression(models.Model):
+    # This should be a evaluatalbe expession hopefully using basic if then logic and true false booleans
+    descript = models.CharField(max_length=200)
+    eval = models.CharField(max_length=200)
+    input = models.ManyToManyField(Input, through='Relation', related_name='expressions')
+    output = models.ManyToManyField(Output, through='Relation', related_name='expressions')
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_used = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.descript
+
+
+class Relation(models.Model):
+    # this is the many-to-many through table
+    input = models.ForeignKey(Input, related_name='relation', on_delete=models.SET_NULL, null=True)
+    output = models.ForeignKey(Output, related_name='relation', on_delete=models.SET_NULL, null=True)
+    express = models.ForeignKey(EvalExpression, related_name='relation', on_delete=models.CASCADE)
