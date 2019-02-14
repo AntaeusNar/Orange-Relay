f66515321feff8fae8646e545555ef66e6028bc3
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-02-14 11:39:01 -0800

    removed fakerpi and references, replaced and updated with OPi.GPIO

--- OR_web_GUI/extendGPIO.py
+++ OR_web_GUI/extendGPIO.py
@@ -1,11 +1,10 @@
-#  This file should only load AFTER FakeRPi.GPIO is loaded, if that one is needed.  Here we are wanted to overide some
-#  functionality of the FakeRPi.GPIO to give a little bit more of an emulation.  honestly this probely needs to be
+#  This file should only load AFTER OPi.GPIO is loaded, if that one is needed.  Here we are wanted to override some
+#  functionality of the OPi.GPIO to give a little bit more of an emulation.  honestly this probably needs to be
 #  forked and rewritten as a separate package for dev work, but here we are.....
 
 # The basic idea is that when a pin is made HIGH or LOW it is writen into a file,
 # and then when the input is checked it reads the file.......
 
-from FakeRPi.GPIO import *
 from . import extendJSON as JSON
 
 
