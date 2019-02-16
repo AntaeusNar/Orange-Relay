fb1b2559eb30615a8f5ea64e17aacb30558e3533
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-02-16 10:50:31 -0800

    restarting conditional logic models and relationships

--- OR_web_GUI/models.py
+++ OR_web_GUI/models.py
@@ -60,23 +60,8 @@ class Rule(models.Model):
 
 # Start of rules v2
 # Currently unused
-
-
-class EvalExpression(models.Model):
-    # This should be a evaluable expression hopefully using basic if then logic and true false booleans
-    descript = models.CharField(max_length=200)
-    eval = models.CharField(max_length=200)
-    input = models.ManyToManyField(Input, through='Relation', related_name='expressions')
-    output = models.ManyToManyField(Output, through='Relation', related_name='expressions')
-    date_added = models.DateTimeField(auto_now_add=True)
-    last_used = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.descript
-
-
-class Relation(models.Model):
-    # this is the many-to-many through table
-    input = models.ForeignKey(Input, related_name='relation', on_delete=models.SET_NULL, null=True)
-    output = models.ForeignKey(Output, related_name='relation', on_delete=models.SET_NULL, null=True)
-    express = models.ForeignKey(EvalExpression, related_name='relation', on_delete=models.CASCADE)
+# rebuilding the rules models as follows:
+#   One Linking statement can have many inputs(each with its own Condition),
+#   one Logic command, and many Outputs(each with its own Condition)
+# E.I. IF (Input #1 is HIGH & Input #3 is LOW)
+#   set (Output # 4 HIGH LATCHED & Output #5 LOW TIMED 10sec & Output #1 Toggle)
